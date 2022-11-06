abe3805a95f7715f5c60bf95cf9daaa9508a71bc
Edwin <edwin.yi.128@gmail.com>
2022-11-05 20:23:04 -0500

    Navbar logo transition

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import { NavLink, Link } from 'react-router-dom'
 import '../stylesheets/navbar.css'
-import logo from '../images/biylogo.png'
+import logoshort from '../images/navbar-logo-short.png'
+import logolong from '../images/navbar-logo-long.png'
 
 export default function NavBar() {
     return (
         <nav>
-            <img id="navbar-logo" src={logo} />
+            <div class="navbar-logo-container">
+                <img class="navbar-logo-long" src={logolong} />
+                <a class="navbar-logo-link" href="/"><img class="navbar-logo-short" src={logoshort} /></a>
+                <div class="overlay"></div>
+            </div>
             <div class="navbar-right-group">
                 <div id="navbar-login">login.</div>
                 <div id="navbar-signup">signup.</div>
