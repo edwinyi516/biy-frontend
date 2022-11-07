cf362ddad48037459c8157feefee2be216345cb4
Edwin <edwin.yi.128@gmail.com>
2022-11-06 21:34:35 -0600

    Home title sub text and animation

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -20,6 +20,7 @@ export default function Home() {
                 <span className="home-title-text-gradient">Take back control</span><br />of how <span className="home-title-text-gradient">you</span> budget
             </div>
             <img src={hometitleimage} id="home-title-image"></img>
+            <div className="title-sub-text">A platform for deliberate budgeteers</div>
         </div>
     )
 }
