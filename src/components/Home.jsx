1806fe477b7ec4607b9c1ae8263321a18c9ba674
Edwin <edwin.yi.128@gmail.com>
2022-11-06 23:31:53 -0600

    Home section 2 created, home title animation adjusted

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,13 +14,23 @@ export default function Home() {
     // console.log(result.substring(0, result.length - 1))
 
     return (
-        <div className="home-container">
-            <div id="stars"></div>
-            <div className="home-title-block">
-                <span className="home-title-text-gradient">Take back control</span><br />of how <span className="home-title-text-gradient">you</span> budget
+        <>
+        <div id="stars"></div>
+            <div className="home-container">
+                <section className="home-section-1">
+                    <div className="home-title-block">
+                        <span className="home-title-text-gradient">Take back control</span><br />of how <span className="home-title-text-gradient">you</span> budget
+                    </div>
+                    <img src={hometitleimage} id="home-title-image"></img>
+                    <div className="title-sub-text">A platform for deliberate budgeteers</div>
+                </section>
+                <section className="home-section-2">
+                    <div className="biy-overview">
+                        <div className="biy-overview-tagline">Financial situations<br />are unique for everyone.<br />Budgeting should be too.</div>
+                        <div className="biy-overview-description">If no two financial situations are alike,<br />why does online budgeting all look the same?<br /><span className="bold">biy.</span> allows users to regain control of <i>how</i><br />they choose to manage their personal finances.</div>
+                    </div>
+                </section>
             </div>
-            <img src={hometitleimage} id="home-title-image"></img>
-            <div className="title-sub-text">A platform for deliberate budgeteers</div>
-        </div>
+        </>
     )
 }
