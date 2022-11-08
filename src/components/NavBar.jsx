53fdc7f58b3a95b130bc2036d4e6158311975cb8
Edwin <edwin.yi.128@gmail.com>
2022-11-08 17:56:25 -0600

    Minor home title block margin adjustment, navbar logo home route conditional

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,30 @@
 import React from 'react'
-import { NavLink, Link } from 'react-router-dom'
+import { NavLink, Link, useLocation } from 'react-router-dom'
 import '../stylesheets/navbar.css'
 import logoshort from '../assets/images/navbar-logo-short-white.png'
 import logolong from '../assets/images/navbar-logo-long-white.png'
 import logob from '../assets/images/navbar-logo-b.png'
 
 export default function NavBar() {
+
+        let location = useLocation()
+        console.log(location.pathname)
     return (
         <nav>
             <div className="navbar-logo-container">
-                <img className="navbar-logo-b" src={logob} />
-                <img className="navbar-logo-long" src={logolong} />
-                <a className="navbar-logo-link" href="/"><img className="navbar-logo-short" src={logoshort} /></a>
-                <div className="overlay"></div>
+            {
+                location.pathname === "/" ? (
+                    <>
+                        <img className="navbar-logo-b" src={logob} />
+                        <img className="navbar-logo-long" src={logolong} />
+                        <a className="navbar-logo-link" href="/"><img className="navbar-logo-short" src={logoshort} /></a>
+                    </>
+                ) : (
+                    <>
+                        <a className="navbar-logo-link" href="/"><img className="navbar-logo-short-static" src={logoshort} /></a>
+                    </>
+                )
+            }
             </div>
             <div className="navbar-right-group">
                 <a id="navbar-login" href="/login">login.</a>
