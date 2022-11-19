955cdab75c7b5dcbc1ef37eb26097b3fc05c5335
Edwin <edwin.yi.128@gmail.com>
2022-11-19 10:11:38 -0600

    Grid item change tests

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -5,14 +5,14 @@ import GridLayout from 'react-grid-layout'
 export default function Dashboard(props) {
 
     const layout = [
-        { i: "a", x: 0, y: 0, w: 1, h: 2, static: true },
-        { i: "b", x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
-        { i: "c", x: 4, y: 0, w: 1, h: 2 }
+        { i: "1", x: 0, y: 0, w: 1, h: 2, static: true },
+        { i: "2", x: 1, y: 0, w: 1, h: 2, minW: 1, maxW: 12, minH: 1, maxH: 12 },
+        { i: "3", x: 4, y: 0, w: 1, h: 2 }
       ];
 
     useEffect(() => {
-        const dashBody = document.querySelector("body")
-        dashBody.style.backgroundColor = "white"
+        const dashboardBody = document.querySelector("body")
+        dashboardBody.style.backgroundColor = "white"
     })
 
     return (
@@ -24,9 +24,9 @@ export default function Dashboard(props) {
                 rowHeight={30}
                 width={1200}
             >
-                <div key="a">a</div>
-                <div key="b">b</div>
-                <div key="c">c</div>
+                <div key="1">Content</div>
+                <div key="2">More content</div>
+                <div key="3">Even more</div>
             </GridLayout>
         </>
     )
