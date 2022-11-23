42cb5f72f446104f49209306ffb18430376f018d
Edwin <edwin.yi.128@gmail.com>
2022-11-23 15:20:12 -0600

    Dashboard grid item margins, shadows, and other basic layout styling set

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,18 +1,27 @@
 import '../../node_modules/react-grid-layout/css/styles.css'
 import '../../node_modules/react-resizable/css/styles.css'
 import '../stylesheets/dashboard.css'
-import React, { useEffect } from 'react' 
+import React, { useState, useEffect } from 'react' 
 import GridLayout from 'react-grid-layout'
 
 export default function Dashboard(props) {
+    const [viewWidth, setViewWidth] = useState()
+    const [rowHeight, setRowHeight] = useState()
+    // const [maxRows, setMaxRows] = useState()
 
     const layout = [
-        { i: "1", x: 0, y: 0, w: 1, h: 2, static: true },
-        { i: "2", x: 1, y: 0, w: 1, h: 2, minW: 1, maxW: 12, minH: 1, maxH: 12 },
+        { i: "1", x: 0, y: 0, w: 1, h: 2 },
+        { i: "2", x: 1, y: 0, w: 2, h: 5, minW: 3, minH: 5 },
         { i: "3", x: 4, y: 0, w: 1, h: 2 }
-      ];
+    ];
 
     useEffect(() => {
+        let width = window.innerWidth - 25
+        let rowHeight = (document.querySelector(".dashboard-container").clientHeight - 25) / 12
+        // let maxRows = 8
+        setViewWidth(width)
+        setRowHeight(rowHeight)
+        // setMaxRows(maxRows)
         document.body.style.overflowY = "hidden"
         return () => {
             document.body.style.overflowY = "scroll"
@@ -28,8 +37,10 @@ export default function Dashboard(props) {
                         className="layout"
                         layout={layout}
                         cols={12}
-                        rowHeight={30}
-                        width={1200}
+                        margin={[25, 25]}
+                        rowHeight={rowHeight}
+                        // maxRows={maxRows}
+                        width={viewWidth}
                     >
                         <div key="1" id="hellogridblock">
                             <div>Hello</div>
